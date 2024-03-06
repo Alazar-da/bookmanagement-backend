ab2eae032b6b5ae223f706d8d2f8566ef8347ccd
alecan27 <alazar.damena01@gmail.com>
2024-03-06 16:03:31 +0300

    changed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-/* const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 //const Product = require('./models/ProductModels');
 const Book = require('./models/BookModels.js');
 
@@ -10,27 +10,29 @@ const cors = require('cors') //install
 
 var corsOptions = {
   origin: "https://book-management-omega.vercel.app"
-}; */
+};
 
 const app = express()
 
-/* app.use(cookieParser())
-app.use(cors(corsOptions)); */
+app.use(cookieParser())
+app.use(cors(corsOptions));
 /* app.use(cors(corsOptions)) */
 
+app.get('/', function (req, res) {
+    res.send('Hello World')
+  })
 
-
-/* var urlencodedParser = express.urlencoded({extended:true})
+var urlencodedParser = express.urlencoded({extended:true})
 
 app.use(urlencodedParser);
 
-app.use(express.json()) */
+app.use(express.json())
 //app.use(cookieParser())
 
-/* app.post('/Book', async (req,res)=>{
-    try{ */
+app.post('/Book', async (req,res)=>{
+    try{
       /* const {name,age} = req.body; */
-   /*    const book = new Book(req.body);
+      const book = new Book(req.body);
       await book.save().then(
         ()=>{
           res.status(200).json(
@@ -48,11 +50,11 @@ app.use(express.json()) */
       }
 
 
-      }) */
+      })
 
 
 
-/* 
+
 mongoose.connect('mongodb+srv://alazar-da:ale123@cluster0.eki1tpt.mongodb.net/bookstore?retryWrites=true&w=majority&appName=Cluster0').then(()=>{
     console.log('connected')
     app.listen(5000,()=>{
@@ -60,14 +62,7 @@ mongoose.connect('mongodb+srv://alazar-da:ale123@cluster0.eki1tpt.mongodb.net/bo
     })
 }).catch((error)=>{
     console.log(error);
-}) */
-
-app.listen(5000,()=>{
-    console.log('node connnected at 5000')
 })
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-  })
 
 module.exports=app
